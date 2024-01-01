b1f8311c923cf0a8f3831472f5171ca3d50b61d8
Guilherme Augusto <guilhermeasn@yahoo.com.br>
2024-01-01 14:23:51 -0300

    calculate fail

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,40 @@
-import { useCallback, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import Board, { BoardData, BoardIndex, BoardPiece } from "./Board";
+import { calculateNextPlay, calculateReversi, startBoard } from "./helpers";
 
 export default function App() {
 
     const [ play, setPlay ] = useState<BoardPiece>('black');
     const [ data, setData ] = useState<BoardData>(Array(8).fill(Array(8).fill('empty')) as BoardData);
 
-    const update = useCallback((...changes : Array<[ BoardIndex, BoardIndex, BoardPiece ]>) : void => {
+    const update = (row : BoardIndex, cell: BoardIndex) : void => {
+        let newData = calculateReversi(data, play, row, cell);
+        let current : BoardPiece = play === 'black' ? 'white' : 'black';
+        setPlay(current);
+        setData(calculateNextPlay(newData, current));
+    }
+
+    useEffect(() => {
 
         setData(data => {
             let newData = data.map(row => row.map(cell => cell === 'play' ? 'empty' : cell)) as BoardData;
-            changes.forEach(([ row, cell, status ]) => newData[row][cell] = status);
-            return CalculateCellPlay(newData, play);
+            startBoard.forEach(([ row, cell, status ]) => newData[row][cell] = status);
+            return calculateNextPlay(newData, play);
         });
 
-        setPlay(play => play === 'black' ? 'white' : 'black');
-        
     }, []); // eslint-disable-line react-hooks/exhaustive-deps
-
-    useEffect(() => update(
-        [ 3, 3, 'white' ],
-        [ 3, 4, 'black' ],
-        [ 4, 3, 'black' ],
-        [ 4, 4, 'white' ]
-    ), [ update ]);
 
     return (
 
         <main>
 
-            <Board data={ data } />
+            <Board
+                data={ data }
+                onClick={ update }
+            />
 
         </main>
 
     )
-
-}
-
-function CalculateCellPlay(data : BoardData, current : BoardPiece) : BoardData {
-
-    const oposite : BoardPiece = current === 'black' ? 'white' : 'black';
-
-    const regexs : Array<[ RegExp, string ]> = [
-        [ new RegExp(`empty-(${oposite}-)+${current}`, 'g'), `play-$1${current}` ], // left to right
-        [ new RegExp(`${current}(-${oposite})+-empty`, 'g'), `${current}$1-play` ], // right to left
-        [ new RegExp(`empty(([-|][a-z]+){7}[-|]${oposite})+(([-|][a-z]+){7}[-|]${current})`, 'g'), `play$1$3` ], // top to bottom
-        [ new RegExp(`${current}(([-|][a-z]+){7}[-|]${oposite})+(([-|][a-z]+){7}[-|])empty`, 'g'), `${current}$1$3play` ], // bottom to top
-    ];
-
-    let board : string = data.map(row => row.join('-')).join('|');
-    regexs.forEach(([ regex, pattern ]) => board = board.replace(regex, pattern));
-    console.log(board.split('|').map(row => row.split('-')));
-    return board.split('|').map(row => row.split('-')) as BoardData;
 
 }
---
+++ src/helpers.ts
@@ -0,0 +1,47 @@
+import type { BoardData, BoardIndex, BoardPiece } from "./Board";
+
+export const startBoard : Array<[ BoardIndex, BoardIndex, BoardPiece ]> = [
+    [ 3, 3, 'white' ],
+    [ 3, 4, 'black' ],
+    [ 4, 3, 'black' ],
+    [ 4, 4, 'white' ]
+];
+
+export function calculateNextPlay(data : BoardData, current : BoardPiece) : BoardData {
+
+    const oposite : BoardPiece = current === 'black' ? 'white' : 'black';
+
+    const replacers : Array<[ RegExp | string, string ]> = [
+        [ 'play', 'empty' ], // clear
+        [ new RegExp(`empty-((${oposite}-)+)${current}`, 'gim'), `play-$1${current}` ], // left to right
+        [ new RegExp(`${current}((-${oposite})+)-empty`, 'gim'), `${current}$1-play` ], // right to left
+        [ new RegExp(`empty(([-|][a-z]+){7}[-|]${oposite})+(([-|][a-z]+){7}[-|]${current})`, 'gim'), `play$1$3` ], // top to bottom
+        [ new RegExp(`${current}(([-|][a-z]+){7}[-|]${oposite})+(([-|][a-z]+){7}[-|])empty`, 'gim'), `${current}$1$3play` ], // bottom to top
+    ];
+
+    let board : string = data.map(row => row.join('-')).join('|');
+    replacers.forEach(([ regex, pattern ]) => board = board.replaceAll(regex, pattern));
+
+    return board.split('|').map(row => row.split('-')) as BoardData;
+
+}
+
+export function calculateReversi(data : BoardData, current : BoardPiece, row : BoardIndex, cell : BoardIndex) : BoardData {
+
+    const oposite : BoardPiece = current === 'black' ? 'white' : 'black';
+    let board : string = data.map((r, kr) => r.map((c, kc) => kr === row && kc === cell ? 'UPDATE' : c).join('-')).join('|');
+
+    const replacers : Array<[ RegExp | string, string ]> = [
+        [ 'play', 'empty' ], // clear
+        [ new RegExp(`(UPDATE.*?)${oposite}(.*?${current})`, 'gim'), `$1${current}$2` ], // left to right
+        [ new RegExp(`(${current}.*?)${oposite}(.*?UPDATE)`, 'gim'), `$1${current}$2` ], // right to left
+        [ 'UPDATE', current ] // updated
+    ];
+
+    replacers.forEach(([ regex, pattern ]) => board = board.replaceAll(regex, pattern));
+
+    console.log(board.split('|').map(row => row.split('-')));
+
+    return board.split('|').map(row => row.split('-')) as BoardData;
+
+}
