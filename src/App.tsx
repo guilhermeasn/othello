84b43df1ad6451e4a89de4676fbc21b8d5c0afb9
Guilherme Augusto <guilhermeasn@yahoo.com.br>
2024-02-29 19:34:27 -0300

    reset button

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,31 @@
-import { useMemo, useState } from "react";
+import { useEffect, useState } from "react";
 import Board from "./Board";
 import Othello, { NextMove } from "./Othello";
 
 export default function App() {
 
-    const othello = useMemo(() => new Othello(), []);
-    const [ nextMove, setNextMove ] = useState<NextMove>(othello.getNextMove());
+    const [ othello, setOthello ] = useState<Othello>(new Othello());
+    const [ nextMove, setNextMove ] = useState<NextMove>(null);
+
+    useEffect(() => setNextMove(othello.getNextMove()), [ othello ]);
 
     return (
 
         <main>
+
             <Board
                 data={ othello.getBoard() }
                 movies={ nextMove === null ? [] : nextMove.moves }
                 onClick={ position => setNextMove(othello.makeMove(position)) }
             />
-            <p><button onClick={ () => setNextMove(othello.backMove()) } disabled={ !othello.isReturnable() }>Back</button></p>
-            <p><button onClick={ () => console.log(othello.getGameString()) }>Salvar</button></p>
-            <p>{ nextMove?.player ?? 'Game End' }</p>
+
+            <div>
+                <p><button onClick={ () => setNextMove(othello.backMove()) } disabled={ !othello.isReturnable() }>Back</button></p>
+                <p><button onClick={ () => console.log(othello.getGameString()) }>Salvar</button></p>
+                <p><button onClick={ () => setOthello(new Othello()) }>Reset</button></p>
+                <p>{ nextMove?.player ?? 'Game End' }</p>
+            </div>
+            
         </main>
 
     )
