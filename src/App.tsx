ccc626860701f6b7e4a20a7b081d700cf1ea4de8
Guilherme Augusto <guilhermeasn@yahoo.com.br>
2024-02-29 12:00:15 -0300

    cleaned

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,13 @@
-import { useEffect, useState } from "react";
-import Board, { BoardData, BoardIndex, BoardPiece } from "./Board";
-import { startBoard } from "./helpers";
 
 export default function App() {
 
-    const [ play, setPlay ] = useState<BoardPiece>('black');
-    const [ data, setData ] = useState<BoardData>(Array(8).fill(Array(8).fill('empty')) as BoardData);
-
-    const update = (row : BoardIndex, cell: BoardIndex) : void => {
-        // let newData = calculateReversi(data, play, row, cell);
-        // let current : BoardPiece = play === 'black' ? 'white' : 'black';
-        // setPlay(current);
-        // setData(calculateNextPlay(newData, current));
-    }
-
-    useEffect(() => {
-
-        setData(data => {
-            let newData = data.map(row => row.map(cell => cell === 'play' ? 'empty' : cell)) as BoardData;
-            startBoard.forEach(([ row, cell, status ]) => newData[row][cell] = status);
-            return newData;
-        });
-
-    }, []); // eslint-disable-line react-hooks/exhaustive-deps
+    
 
     return (
 
         <main>
 
-            <Board
-                data={ data }
-                onClick={ update }
-            />
+            
 
         </main>
 
