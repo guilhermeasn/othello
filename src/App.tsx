d5bb6299a7a9e9baa98be5ca82344ca3fdb4419a
Guilherme Augusto <guilhermeasn@yahoo.com.br>
2024-02-29 21:04:13 -0300

    styled

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { useEffect, useState } from "react";
 import Board from "./Board";
-import Othello, { NextMove } from "./Othello";
+import Othello, { NextMove, Status } from "./Othello";
 
 export default function App() {
 
     const [ othello, setOthello ] = useState<Othello>(new Othello());
     const [ nextMove, setNextMove ] = useState<NextMove>(null);
+
+    const status = othello.getStatus();
 
     useEffect(() => setNextMove(othello.getNextMove()), [ othello ]);
 
@@ -13,17 +15,35 @@ export default function App() {
 
         <main>
 
+            <div className="player">
+                { nextMove ? (
+                    <>
+                        <div className={ `board-${ nextMove.player }` }></div>
+                        <div className="strong">{ nextMove.player }&nbsp;player</div>
+                    </>
+                ) : (
+                    <div className="strong red">Game End</div>
+                ) }
+            </div>
+
+            <div className="status">
+                { Object.keys(status).map(k => (
+                    <div className="strong" key={ k }>
+                        { k }:&nbsp;{ status[k as keyof Status] }
+                    </div>
+                )) }
+            </div>
+
             <Board
                 data={ othello.getBoard() }
                 movies={ nextMove === null ? [] : nextMove.moves }
                 onClick={ position => setNextMove(othello.makeMove(position)) }
             />
 
-            <div>
+            <div className="buttons">
                 <p><button onClick={ () => setNextMove(othello.backMove()) } disabled={ !othello.isReturnable() }>Back</button></p>
-                <p><button onClick={ () => console.log(othello.getGameString()) }>Salvar</button></p>
+                {/* <p><button onClick={ () => console.log(othello.getGameString()) }>Salvar</button></p> */}
                 <p><button onClick={ () => setOthello(new Othello()) }>Reset</button></p>
-                <p>{ nextMove?.player ?? 'Game End' }</p>
             </div>
             
         </main>
